2e413fc58ab8d2919f24b1efaf9cf0ebdcc73e3a
Forvater <justmail12345@gmail.com>
2013-04-04 12:24:07 +0400

    added mouse functions for lmb and rmb

---
+++ touch_monitor_console_test_program/mouse_functions.h
@@ -0,0 +1,11 @@
+#ifndef MOUSE_FUNCTIONS_H
+#define MOUSE_FUNCTIONS_H
+
+void mouse_rmb_click(long x, long y);
+void mouse_lmb_click(long x, long y);
+
+
+void mouse_lmb_down(long x, long y);
+void mouse_lmb_up(long x, long y);
+
+#endif
--- touch_monitor_console_test_program/screen_coordinates_processing.cpp
+++ touch_monitor_console_test_program/screen_coordinates_processing.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <Windows.h>
+#include "mouse_functions.h"
 
 double screen_x = 0.0;
 double screen_y = 0.0;
@@ -9,6 +10,10 @@ double screen_x_ent_zone = 0.0; // x coor of entering zone point
 double screen_y_ent_zone = 0.0; // y coor of entering zone point
 double screen_x_lv_zone = 0.0; // x coor of leaving zone point
 double screen_y_lv_zone = 0.0; // y coor of leaving zone point
+unsigned long enter_zone_time = 0; // milliseconds from machine start
+unsigned long leave_zone_time = 0; // milliseconds from machine start
+unsigned long inside_zone_time = 0;
+
 
 void enter_zone_event_handler();
 void leave_zone_event_handler();
@@ -42,11 +47,22 @@ void print_coordinates_and_set_cursor(double x, double y) {
 }
 
 void enter_zone_event_handler() {
+  enter_zone_time = GetTickCount();
+  mouse_lmb_down(screen_x_ent_zone,screen_y_ent_zone);
+  // SetTimer
   printf("ent X:  %4.2f\r\n", screen_x_ent_zone);
   printf("ent Y:  %4.2f\r\n", screen_y_ent_zone);
 }
 
 void leave_zone_event_handler() {
+  leave_zone_time = GetTickCount();
+  inside_zone_time = leave_zone_time - enter_zone_time;
+  mouse_lmb_up(screen_x_lv_zone, screen_y_lv_zone);
+//  if(inside_zone_time >= 1000) {
+//    mouse_rmb_click(screen_x_lv_zone,screen_y_lv_zone);
+//    mouse_lmb_click(screen_x_lv_zone,screen_y_lv_zone);
+//  }
   printf("lv X:  %4.2f\r\n", screen_x_lv_zone);
   printf("lv Y:  %4.2f\r\n", screen_y_lv_zone);
+  printf("time inside zone:  %d\r\n", inside_zone_time);
 }
